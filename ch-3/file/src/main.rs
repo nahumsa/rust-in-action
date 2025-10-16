c19df105f0d707026800e178874e5ee2809910ac
nahumsa <nahumsaa@gmail.com>
2025-10-16 18:42:20 -0300

    feat: add ch-3

---
+++ ch-3/file/src/main.rs
@@ -0,0 +1,86 @@
+#[allow(dead_code)]
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Close,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Close => write!(f, "CLOSED"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct File {
+    pub name: String,
+    data: Vec<u8>,
+    pub state: FileState,
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "<{}, {}>", self.name, self.state)
+    }
+}
+
+impl File {
+    pub fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Close,
+        }
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
+
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+
+        Ok(read_length)
+    }
+}
+
+fn open(mut f: File) -> Result<File, String> {
+    f.state = FileState::Open;
+    Ok(f)
+}
+
+fn close(mut f: File) -> Result<File, String> {
+    f.state = FileState::Close;
+    Ok(f)
+}
+
+fn main() {
+    let mut f5 = File::new("f5.txt");
+
+    let mut buffer: Vec<u8> = vec![];
+
+    if f5.read(&mut buffer).is_err() {
+        println!("Error checking is working");
+    }
+
+    f5 = open(f5).unwrap();
+    let f5_length = f5.read(&mut buffer).unwrap();
+    f5 = close(f5).unwrap();
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f5);
+    println!("{}", f5);
+    println!("{} is {} bytes long", &f5.name, f5_length);
+    println!("{}", text);
+}
